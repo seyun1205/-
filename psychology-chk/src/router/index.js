d330ce250bbdc840105a22422cdc6f44b452931a
kaifuren <115054835+kaifuren@users.noreply.github.com>
2022-11-12 21:39:37 +0800

    Add files via upload

--- psychology-chk/src/router/index.js
+++ psychology-chk/src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/login',
     name: 'login',
     component: () => import('../views/from/login.vue')
   },
@@ -19,7 +19,33 @@ const routes = [
     path: '/manage',
     name: 'manage',
     component: () => import('../views/behind/manage.vue')
-  }
+  },
+
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('../views/from/index.vue'),
+     }, 
+      {
+        path: '/test',
+        name: 'test',
+        component: () => import('../views/from/main/test.vue')
+      },
+      {
+        path: '/test2',
+        name: 'test2',
+        component: () => import('../views/from/main/test2.vue')
+      },
+      {
+        path: '/judge/:grade',
+        name: 'judge',
+        component: () => import('../views/from/main/judge.vue')
+      },
+      {
+        path:'*',  //URL输入错误或者是URL匹配不到静态资源，会自动跳转到主页
+        redirect:'/'
+      }
+  
 ]
 
 const router = new VueRouter({
