35055028f39da5b88469829cf591b11c5b988fe4
周伟博 <84439181+seyun1205@users.noreply.github.com>
2022-11-15 22:49:35 +0800

    Update knowledge.vue

--- psychology-chk/src/views/from/main/knowledge.vue
+++ psychology-chk/src/views/from/main/knowledge.vue
@@ -1,15 +1,132 @@
 <template>
-  <div>
-    知识页面
+  <div class="all">
+    <div class="body_title">
+      <h1 class="title">心理知识查询</h1>
+      <div class="search">
+        <input class="search_input" type="text" placeholder="请输入想要查询的心理知识"></input>
+        <el-button icon="el-icon-search" circle></el-button>
+      </div>
+      <div class="search_codes">
+        <el-select v-model="select" slot="prepend" placeholder="请选择">
+          <el-option label="餐厅名" value="1"></el-option>
+          <el-option label="订单号" value="2"></el-option>
+          <el-option label="用户电话" value="3"></el-option>
+        </el-select>
+      </div>
+    </div>
+
+
+
+    <div class="body_views">
+      <div class="body_views_view">
+          views
+      </div>
+      <div class="body_views_papers">
+        <div class="body_views_papers_li">
+          papers
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      <el-button-group class="footer_button">
+        <el-button type="primary" icon="el-icon-arrow-left">上一页</el-button>
+        <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+      </el-button-group>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "knowledge"
+  name: "knowledge",
+  data() {
+    return {
+      input1: '',
+      input2: '',
+      input3: '',
+      select: ''
+    }
+  }
 }
 </script>
 
 <style scoped>
+  div{
+    display: block;
+  }
+  .all{
+    width: 100%;
+    height: 100%;
+    background-image: radial-gradient(lightblue, white);
+  }
 
+ .body_title{
+   margin: 0 auto;
+   box-sizing: border-box;
+   flex-direction: row;
+   padding-top: 80px;
+   text-align: center;
+ }
+ .title{
+   text-align: center;
+   padding: 0px;
+   margin-bottom:20px;
+ }
+  .search{
+    padding: 0px;
+    margin: 0 auto;
+    width: 60%;
+    background-color: white;
+    border-radius: 4px;
+  }
+  .search_input{
+    font: inherit;
+    letter-spacing: inherit;
+    color: currentcolor;
+    padding: 4px 5px 5px;
+    box-sizing: content-box;
+    background: none;
+    height: 2em;
+    width: 90%;
+    margin: 10px 4px 10px;
+    border-radius: 4px;
+    border: 2px solid #B3C0D1;
+  }
+  .search_codes{
+
+  }
+  
+  .body_views{
+    box-sizing: border-box;
+    display: flex;
+    flex-flow: row wrap;
+    width: 100%;
+    height: 65%;
+    -webkit-box-pack: center;
+    justify-content: center;
+  }
+  .body_views_view{
+    text-align: center;
+    padding: 0px;
+    margin: 0 auto;
+  }
+  .body_views_papers{
+    text-align: center;
+    padding: 0;
+    margin: 0 auto;
+  }
+  .footer{
+    margin-bottom: 10px;
+    padding: 0;
+    text-align: center;
+    width: 100%;
+
+  }
+  .footer_button{
+    text-align: center;
+    padding: 0;
+    margin: 0 auto;
+  }
 </style>
